7b85fe0dcc4d1a87847ef78e74d6d2793f55e550
halldenham <hall.denham@gmail.com>
2017-01-23 15:15:25 -0500

    Add files via upload

--- Est_Hrs_by_week2.py
+++ Est_Hrs_by_week2.py
@@ -14,21 +14,33 @@ import matplotlib
 from datetime import datetime, timedelta, date
 
 # Location of file with data
-Location = r'C:\Users\dh1023.AD\Desktop\Python\Zone_Manager_Report_Open_and_Closed_WO_-_DGH_Master.xlsx'
+# Criteria for Report:
+# Req Type = "PM; SR"
+# WO Status = "Assigned; Open"
+# Crews = "All"
 
+location = r'C:\Users\dh1023.AD\Desktop\Python\Zone_Manager_Report_Open_and' \
+            '_Closed_WO_-_DGH_Master.xlsx'
+
+''' 
+# alternate location for Projected SR
+#location = r'C:\Users\dh1023.AD\Desktop\Python\Projected SR.xlsx'            
+'''
+            
 # Parse a specific sheet
-df = pd.read_excel(Location)
+df = pd.read_excel(location)
 
 # create the dataframe to write data to
 x_Date = []
 x_Hrs = []
-x_WOnum = []
+x_wo_num = []
 x_SRnum = []
 x_Crew = []
 x_Craft = []
 x_Priority = []
-x_dataSet = list(zip(x_Date,x_Hrs,x_WOnum,x_SRnum,x_Crew,x_Craft,x_Priority))
-x_df = pd.DataFrame(data = x_dataSet, columns=['Date', 'Est Hrs', 'WO Num', 'SR Num'])
+x_dataSet = list(zip(x_Date,x_Hrs,x_wo_num,x_SRnum,x_Crew,x_Craft,x_Priority))
+x_df = pd.DataFrame(data = x_dataSet, columns=['Date', 'Est Hrs', 'WO Num', 
+                                               'SR Num'])
 
 
 today = pd.to_datetime(date.today())
@@ -38,48 +50,56 @@ i = 0 # row of data we're pulling
 while i < len(df):
     
     # select start and due dates
-    dueDate = df.iloc[i,3]    
-    enterDate = df.iloc[i,2]
-    avgHrs = df.iloc[i,1]
-    WOnum = df.iloc[i,0]
-    SRnum = df.iloc[i,4]
-    Crew = df.iloc[i,5]
-    Craft = df.iloc[i,6]
-    Priority = df.iloc[i,7]
+    wo_num = df.iloc[i,0]
+    avg_hrs = df.iloc[i,1]
+    enter_date = df.iloc[i,2]
+    due_date = df.iloc[i,3]    
+    sr_num = df.iloc[i,4]
+    crew = df.iloc[i,5]
+    craft = df.iloc[i,6]
+    priority = df.iloc[i,7]
 
     # select the earliest date available to work
-    if dueDate <= today:
+    if due_date <= today:
         # append only 1 row of data if it's already past due
-        dueDateMonday = (dueDate - timedelta(days=dueDate.weekday()))
-        aa = pd.DataFrame([[dueDateMonday, avgHrs, WOnum, SRnum, Crew, Craft, Priority]], columns=['Date', 'Est Hrs', 'WO Num', 'SR Num', 'Crew', 'Craft', 'Priority'])
+        due_date_mon = (due_date - timedelta(days=due_date.weekday()))
+        aa = pd.DataFrame([[due_date_mon, avg_hrs, wo_num, sr_num, crew, craft, 
+                            priority]], columns=['Date', 'Est Hrs', 'WO Num', 
+                                                 'SR Num', 'crew', 'craft', 
+                                                 'priority'])
         x_df = x_df.append(aa, ignore_index=True)
         
     else:
         
         # convert start and due dates to the Monday of the week they fall in
         
-        # if the WO "Start Date" is prior to today, and is due after today, then 
-        # we need to select the available hours for only the weeks we still have
-        # available to work. In other words, don't put estimated time to prior
-        # weeks if the WO is still open and not overdue
-        todayMonday = (today - timedelta(days=today.weekday()))
-        originalMonday = (enterDate - timedelta(days=enterDate.weekday()))
-        monday1 = max(todayMonday,originalMonday)
-        monday2 = (dueDate - timedelta(days=dueDate.weekday()))
+        # if the WO "Start Date" is prior to today, and is due after today, 
+        # then we need to select the available hours for only the weeks we 
+        # still have available to work. In other words, don't put estimated 
+        # time to prior weeks if the WO is still open and not overdue
+        today_mon = (today - timedelta(days=today.weekday()))
+        original_mon = (enter_date - timedelta(days=enter_date.weekday()))
+        monday1 = max(today_mon,original_mon)
+        monday2 = (due_date - timedelta(days=due_date.weekday()))
         
         # list of the mondays available for the WO to be completed
-        dfWeeksBetween = pd.date_range(monday1, monday2, freq='W-Mon')
+        df_weeks_between = pd.date_range(monday1, monday2, freq='W-Mon')
 
         # different information to write back to our data
-        weeksBetween = 1 + (monday2 - monday1).days / 7
+        weeks_between = 1 + (monday2 - monday1).days / 7
         
         # find average hours per week
-        avgHrsPerWeek = avgHrs / weeksBetween        
+        avg_hrs_per_week = avg_hrs / weeks_between        
     
-        # now write Avg Hrs per week to each of the mondays between monday1 and monday 2
+        # now write Avg Hrs per week to each of the mondays between monday1 
+        # and monday 2
         # write those into a dataframe and then append it to the original data
-        for x in dfWeeksBetween: 
-            aa = pd.DataFrame([[x, avgHrsPerWeek, WOnum, SRnum, Crew, Craft, Priority]], columns=['Date', 'Est Hrs', 'WO Num', 'SR Num', 'Crew', 'Craft', 'Priority'])
+        for x in df_weeks_between: 
+            aa = pd.DataFrame([[x, avg_hrs_per_week, wo_num, sr_num, crew, craft, 
+                                priority]], columns=['Date', 'Est Hrs', 
+                                                     'WO Num', 'SR Num', 
+                                                     'crew', 'craft', 
+                                                     'priority'])
             x_df = x_df.append(aa, ignore_index=True)
 
     # loop through all the rows    
@@ -87,4 +107,10 @@ while i < len(df):
 
 # export results to Excel   
 x_df.to_excel('Estimated Hrs per week.xlsx', index=False)
+
+'''
+# alternate location for taking the SR projected
+#x_df.to_excel('Estimated Hrs per week SR Proj.xlsx', index=False)
+'''
+
 print('Done')
